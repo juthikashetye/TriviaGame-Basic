83e07eee6ae269a4856f1410699c737d20bab364
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-01-25 12:26:46 -0800

    Timer countdown,stop done with updating content div after stopping

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+var countdown = 5;
+var correctAns = 0;
+var wrongAns = 0;
+var notAns = 0;
+var runTimer;
+
+//Starts the countdown timer
+function run() {
+	//Clearing the runTimer prior to setting 
+	//our new runTimer will not allow multiple instances.
+      clearInterval(runTimer);
+      runTimer = setInterval(timer, 1000);
+    }
+
+function timer(){
+	//Decrease the countdown
+	countdown --;
+	//Update text of countdown holder span tag
+	$("span").text(countdown);
+
+	//Once timer hits zero, stop timer
+	//update the content div
+	if (countdown===0) {
+		//Call the stop function to stop the timer
+		stop();
+		//Create 'p' tags with updated text
+		//and append to content div
+		var result = $("<p>" + "<p>" + "<p>");
+		result.eq(0).text("Correct answers : "+ correctAns);
+		result.eq(1).text("Inorrect answers : "+ wrongAns);
+		result.eq(2).text("Unanswered : "+ notAns);
+		$("#content").append(result);
+ 	}
+}
+
+//Stops the timer
+function stop() {
+    //  Clears our runTimer
+    clearInterval(runTimer);
+}
+
+//Call the run function in order to start the timer    
+run();
